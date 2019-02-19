37472d9b874238822249d01c0e322c65515c229f
Dmitry <lbvfyf2@mail.ru>
2019-02-19 12:57:56 +0300

    lab2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int A=int.Parse(Console.ReadLine());
-
+            int B = int.Parse(Console.ReadLine());
         }
     }
 }
